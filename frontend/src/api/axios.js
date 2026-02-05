acfa96b38d734efb2012ec1ad2ee9f885c301c9b
Nuthan Sagar <siddubade65@gmail.com>
2026-02-05 18:30:07 +0530

    Fix: Api fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
